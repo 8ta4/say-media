fb89b1974157876be5db5d65e62ed9a37cb9c9a7
a <a@as-Virtual-Machine.local>
2025-02-02 00:25:10 -0800

    Enable gitleaks pre-commit hook with verbose and redact options

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,10 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [
+    pkgs.git
+    pkgs.gitleaks
+  ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
@@ -40,6 +43,20 @@
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
+  pre-commit.hooks = {
+    gitleaks = {
+      enable = true;
+      # https://github.com/gitleaks/gitleaks/blob/48a2e0e3184d8d07275a22f24d3c96019df8cd4c/.pre-commit-hooks.yaml#L4
+      entry = "gitleaks protect --verbose --redact --staged";
+    };
+    nixpkgs-fmt.enable = true;
+    trailing-whitespace = {
+      enable = true;
+      # https://github.com/pre-commit/pre-commit-hooks/blob/fd011242fd472708233388ea6d68964469e3a774/.pre-commit-hooks.yaml#L208-L210
+      entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/trailing-whitespace-fixer";
+      types = [ "text" ];
+    };
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
